62d19e10fc3fd300d52a2df669d2bb0e59f3a152
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-22 09:29:36 +0530

    append the simple createElement

---
+++ script.js
@@ -0,0 +1,19 @@
+//Step 1: Creating a html Element
+//syntax : let/const variablename = document.createElement("Elementname/htmltag")
+//Step 2: Adding the content (innerHTML & innerText)
+// syntax: variablename.innerHTML / innerText = "Content"
+//Step 3: Attribute (SetAttribute / GetAttribute)
+// syntax: varibalename.setAttribute("attributename","attributevalue") 
+//example : heading.setAttribute("id","head")
+//example : heading.setAttribute("class","head")
+//heading.classlist.add("text-center","text-2xl")
+//heading.classlist.remove("text-center")
+//Step 4:- Appending the element to html document (append & appendchild)
+//syntax: document.body.append(variablename)
+//syntax: variablename.appendchild(variablename)
+
+//Example
+
+const heading=document.createElement("h1")
+heading.innerHTML="<i>Hello World</i>"
+document.body.append(heading)
